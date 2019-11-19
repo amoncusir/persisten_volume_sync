df01d4df19e0af01cdb749d2234f5b5afc468b69
Aran <amoncusir@gmail.com>
2019-11-19 21:32:20 +0100

    Touch files structure

---
+++ manage.py
@@ -0,0 +1,9 @@
+#
+# Digital Poet
+#
+# Aran Moncusí Ramírez <aran@digitalpoet.info>
+#
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
---
+++ persisten_sync/__init__.py
@@ -0,0 +1,5 @@
+#
+# Digital Poet
+#
+# Aran Moncusí Ramírez <aran@digitalpoet.info>
+#
