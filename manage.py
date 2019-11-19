e6f50ba8245e200d52c75a4e152dea6afb7799ce
Aran <amoncusir@gmail.com>
2019-11-19 22:17:25 +0100

    Init script ended

--- manage.py
+++ manage.py
@@ -6,4 +6,10 @@
 import argparse
 
 if __name__ == '__main__':
+    """
+    Commands:
+     - init
+     - pull
+     - push
+    """
     parser = argparse.ArgumentParser()
--- persisten_sync/init_repo.py
+++ persisten_sync/init_repo.py
@@ -3,3 +3,28 @@
 #
 # Aran Moncusí Ramírez <aran@digitalpoet.info>
 #
+# Check if repo is init
+# If not, clone repo
+# git checkout to selected branch
+#
+import git.repo.fun
+from git import Repo
+
+
+def is_directory_init(path: str):
+    return git.repo.fun.is_git_dir(path)
+
+
+def clone_repository(path: str, repository_source: str, branch_name) -> Repo:
+    return git.Repo.clone_from(to_path=path, url=repository_source, multi_options=('--recurse-submodules',),
+                               branch=branch_name)
+
+
+def init(path, repository, branch_name):
+
+    if not is_directory_init(path):
+        clone_repository(path, repository, branch_name)
+
+
+# if __name__ == '__main__':
+#     init("../t", "git@github.com:EndermanAPM/stock_updater.git", 'devel')
